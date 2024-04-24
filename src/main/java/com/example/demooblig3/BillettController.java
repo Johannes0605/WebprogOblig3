12300c0e650b871c28e73b715f43b630a645c755
Johannes0605 <143514407+Johannes0605@users.noreply.github.com>
2024-04-24 16:26:59 +0200

    Initial commit

---
+++ src/main/java/com/example/demooblig3/BillettController.java
@@ -0,0 +1,38 @@
+package com.example.oblig3data1700;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+
+
+    @RestController
+    public class BillettController{
+
+        @Autowired
+        private BillettRepository rep;
+
+        @PostMapping("/lagre")
+        public void lagreBillett(billett innBillett) {
+            rep.lagreBillett(innBillett);
+        }
+
+        @GetMapping("/hentBilletter")
+        public List <billett> hentBilletter(){
+            return rep.hentAlleBilletter();
+        }
+        @GetMapping("/slett")
+        public void slett(){
+            rep.slettbillett();
+        }
+        @GetMapping("/slettEnBillett")
+        public void slettEnBillett (@RequestParam("id") int id){
+            rep.slettEnBillett(id);
+        }
+    }
+
---
+++ src/test/java/com/example/demooblig3/DemoOblig3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demooblig3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOblig3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
